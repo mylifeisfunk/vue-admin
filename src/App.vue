c28f18881efa6c87a4103185b81b1e0ca46f4a79
“mylifeisfunk <565769916@qq.com>
2020-10-22 01:33:35 +0800

    构建项目,配置插件

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">Fight!!
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {}
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <div id="login">123132</div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style lang="scss" scoped>
+ #login{
+   background: 100vh;
+ }
+</style>
